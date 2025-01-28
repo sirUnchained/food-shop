c5999cd65bf49a918a3aeb83a38a77867b08552a
sirUnchained <bbehnam3831@gmail.com>
2025-01-28 17:23:10 +0330

    you know i sometimes miss cpp, i allways used comments when i was coding cpp and now it seems like i have to use a lot of comments in golang

--- src/services/category.service.go
+++ src/services/category.service.go
@@ -1,7 +1,6 @@
 package services
 
 import (
-	"fmt"
 	"foodshop/api/dto"
 	"foodshop/api/helpers"
 	"foodshop/data/models"
@@ -20,9 +19,9 @@ func GetCategoryService() *categoryService {
 
 func (cs *categoryService) GetAll(ctx *gin.Context) *helpers.ResultResponse {
 	var categories []models.Category
-
 	db := postgres.GetDb()
 
+	// get all categories with relation users, ignore some of those users fields.
 	err := db.Model(&models.Category{}).Preload("Creator", func(db *gorm.DB) *gorm.DB {
 		return db.Select("id", "user_name", "email")
 	}).Find(&categories).Error
@@ -36,37 +35,35 @@ func (cs *categoryService) GetAll(ctx *gin.Context) *helpers.ResultResponse {
 
 func (cs *categoryService) Create(ctx *gin.Context) *helpers.ResultResponse {
 	db := postgres.GetDb()
-
+	// get and check current logged admin
 	creator, exist := ctx.Get("user")
 	if !exist {
 		return &helpers.ResultResponse{Ok: false, Status: 500, Message: "something went wrong.", Data: nil}
 	}
-
-	fmt.Println(creator)
-
+	// get entered inputs, if there was err then return it
 	var input dto.CategoryDTO
 	if err := ctx.ShouldBindJSON(&input); err != nil {
 		return &helpers.ResultResponse{Ok: false, Status: 400, Message: "invalid input!", Data: nil}
 	}
-
+	// create slug
 	slug := strings.Replace(input.Title, " ", "-", -1)
-
+	// check current slug exists
 	var checkCategory models.Category
 	if err := db.Model(&models.Category{}).Where("slug = ?", slug).First(&checkCategory).Error; err == nil {
 		if checkCategory.ID != 0 {
 			return &helpers.ResultResponse{Ok: false, Status: 400, Message: "category title is duplicated.", Data: nil}
 		}
 	}
-
+	// create slug model
 	newCategory := models.Category{
 		Title:     input.Title,
 		Slug:      slug,
 		CreatorID: creator.(models.Users).ID,
 	}
-
+	// create slug in db, i hop there is no err
 	if err := db.Create(&newCategory).Error; err != nil {
 		return &helpers.ResultResponse{Ok: false, Status: 500, Message: err.Error(), Data: nil}
 	}
-
-	return &helpers.ResultResponse{Ok: true, Status: 201, Message: "category created", Data: newCategory}
+	// return done.
+	return &helpers.ResultResponse{Ok: true, Status: 201, Message: "category created,", Data: newCategory}
 }
