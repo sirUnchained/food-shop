8e089be154aab80f43549aa4f415a90b7c554a83
sirUnchained <bbehnam3831@gmail.com>
2025-01-29 15:54:11 +0330

    success ! our route proection worked.

--- src/api/routers/category.routes.go
+++ src/api/routers/category.routes.go
@@ -11,7 +11,7 @@ func CategoryRoutes(r *gin.RouterGroup) {
 	cc := controllers.GetCategoryController()
 
 	r.GET("/category", cc.GetAll)
-	r.POST("/category", middlewares.AuthorizeUser(), cc.Create)
-	r.PATCH("/category/:id", middlewares.AuthorizeUser(), cc.Update)
-	r.DELETE("/category/:id", middlewares.AuthorizeUser(), cc.Remove)
+	r.POST("/category", middlewares.AuthorizeUser(), middlewares.RoleGaurd("admin"), middlewares.RoleGaurd("admin"), cc.Create)
+	r.PATCH("/category/:id", middlewares.AuthorizeUser(), middlewares.RoleGaurd("admin"), cc.Update)
+	r.DELETE("/category/:id", middlewares.AuthorizeUser(), middlewares.RoleGaurd("admin"), cc.Remove)
 }
