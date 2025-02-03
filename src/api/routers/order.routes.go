8b522291074989f00f8ea19a9227b6ecea7935e8
sirUnchained <bbehnam3831@gmail.com>
2025-02-03 14:01:18 +0330

    .

--- src/api/routers/order.routes.go
+++ src/api/routers/order.routes.go
@@ -11,7 +11,7 @@ func OrderRoutes(r *gin.RouterGroup) {
 	rc := controllers.GetOrderController()
 
 	r.GET("/orders", middlewares.AuthorizeUser(), middlewares.RoleGaurd("admin"), rc.GetAll)
-	r.GET("/orders", middlewares.AuthorizeUser(), middlewares.RoleGaurd("admin"), rc.Create)
+	r.POST("/orders", middlewares.AuthorizeUser(), middlewares.RoleGaurd("admin"), rc.Create)
 	r.PATCH("/orders/deliver-status", middlewares.AuthorizeUser(), middlewares.RoleGaurd("admin"), rc.DeliveredStatus)
 	r.PATCH("/orders/star", middlewares.AuthorizeUser(), middlewares.RoleGaurd("admin"), rc.AddStars)
 	r.DELETE("/orders/:id", middlewares.AuthorizeUser(), middlewares.RoleGaurd("admin"), rc.Remove)
