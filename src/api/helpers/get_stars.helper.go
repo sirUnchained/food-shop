2e09f4d0d56da85bf7571fe5a8825b94fd3fdc3b
sirUnchained <bbehnam3831@gmail.com>
2025-02-04 15:25:51 +0330

    added calculate stars function

--- src/api/helpers/get_stars.helper.go
+++ src/api/helpers/get_stars.helper.go
@@ -1,1 +1,18 @@
 package helpers
+
+import (
+	"foodshop/data/models"
+	"foodshop/data/postgres"
+)
+
+func GetCalcRestaurantStars(restaurantID int) int {
+	db := postgres.GetDb()
+	var result struct {
+		Count int
+		Sum   int
+	}
+
+	db.Model(&models.Orders{}).Where("restaurant_id = ? AND stars != 0", restaurantID).Select("COUNT(*) as count, SUM(stars) as sum").Scan(&result)
+
+	return result.Sum / result.Count
+}
